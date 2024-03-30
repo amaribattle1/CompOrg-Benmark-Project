e3516d95cd24d3f726a68e44c72cf7b00ce1828b
ambat1 <12321379-amaribattle1@users.noreply.replit.com>
2024-03-30 02:29:14 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CompOrg-Benmark-Project/benchmark.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void intBenchmark(){
+ 
+}
+
+void floatBenchmark(){
+  
+}
+
+void memBenchmark(){
+  
+}
+
+void hardDriveBenchmark1(){
+  
+}
+
+void hardDriveBenchmark2(){
+
+}
+
+
+
+int main() {
+  intBenchmark();
+  floatBenchmark();
+  hardDriveBenchmark1();
+  hardDriveBenchmark2();
+
+  return 0;
+}
