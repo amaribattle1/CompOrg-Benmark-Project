0cbd892ab8fae43ba9719572ab2eca5042d2eabf
amaribattle1 <124077929+amaribattle1@users.noreply.github.com>
2024-03-30 03:23:34 +0000

    added the chrono library to time and the cmath library to calculate the powers and added the 10^10 additions (of integer constants) 5 × 10^9 multiplication (of integer constants) 2 × 10^9 division (of integer constants)

--- .ccls-cache/@home@runner@CompOrg-Benmark-Project/benchmark.cpp
+++ .ccls-cache/@home@runner@CompOrg-Benmark-Project/benchmark.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void intBenchmark(){
- 
+  
 }
 
 void floatBenchmark(){
--- benchmark.cpp
+++ benchmark.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <chrono>
+#include <cmath>
 using namespace std;
+using namespace std::chrono; 
 
 void intBenchmark(){
- 
+  const int ADDITIONS = pow(10,9);
+  const long long MULTIPLICATIONS = (5 * pow(10,9)); // 5 billion
+  const long long DIVISIONS = (2* pow(10,9)); // 2 billion
 }
 
 void floatBenchmark(){
